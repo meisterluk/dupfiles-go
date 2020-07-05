2dc2d72c8cab1aac84f1b6901320da268b88e217
meisterluk <admin@lukas-prokop.at>
2020-07-06 00:59:15 +0200

    [API+README] More public information.

--- v1/api.go
+++ v1/api.go
@@ -7,7 +7,11 @@ import (
 const VERSION_MAJOR = 1
 const VERSION_MINOR = 0
 const VERSION_PATCH = 0
+const SPEC_MAJOR = 1
+const SPEC_MINOR = 0
+const SPEC_PATCH = 0
 const RELEASE_DATE = "2020-04-29"
+const LICENSE = `BSD 3-clause`
 
 func GenerateReport(ReportParameters) error {
 	return fmt.Errorf(`not implemented yet`)
